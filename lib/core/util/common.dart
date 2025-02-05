f365835cbc5cad9bfbb026d7a4fea1b41ad019c1
Yuval Arbel <Arbelyuval56@gmail.com>
2025-02-05 23:35:42 +0200

    start working on signin and signup page

---
+++ lib/core/util/common.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import 'colors.dart';
+
+
+InputDecoration waInputDecoration({IconData? prefixIcon, String? hint, Color? bgColor, Color? borderColor, EdgeInsets? padding}) {
+  return InputDecoration(
+    contentPadding: padding ?? EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+    counter: Offstage(),
+    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(16), borderSide: BorderSide(color: borderColor ?? WAPrimaryColor)),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16)),
+      borderSide: BorderSide(color: Colors.grey.withOpacity(0.2)),
+    ),
+    fillColor: bgColor ?? WAPrimaryColor.withOpacity(0.04),
+    hintText: hint,
+    prefixIcon: prefixIcon != null ? Icon(prefixIcon, color: WAPrimaryColor) : null,
+    hintStyle: secondaryTextStyle(),
+    filled: true,
+  );
+}

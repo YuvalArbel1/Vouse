9a27e31904673af4a8e8e7bbb7aaa9ec2037112c
Yuval Arbel <Arbelyuval56@gmail.com>
2025-03-06 20:06:15 +0200

    changing the project hierarchy

--- lib/presentation/providers/post/post_text_provider.dart
+++
@@ -1,9 +0,0 @@
-// lib/presentation/providers/post/post_text_provider.dart
-
-import 'package:riverpod/riverpod.dart';
-
-/// A simple [StateProvider] that holds the user's current post text.
-///
-/// Default value is an empty string, representing no text typed yet.
-/// ```
-final postTextProvider = StateProvider<String>((ref) => '');

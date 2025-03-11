ab381b6defd7835e81f7c0472c9e62d67bde74ba
Yuval Arbel <Arbelyuval56@gmail.com>
2025-03-11 18:04:38 +0200

    Connecting the server to the flutter app

--- vouse_flutter/ios/Runner/AppDelegate.swift
+++ vouse_flutter/ios/Runner/AppDelegate.swift
@@ -1,13 +1,50 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore
+import FirebaseMessaging
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
+
+    // Register for remote notifications
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
+    } else {
+      let settings: UIUserNotificationSettings = UIUserNotificationSettings(
+        types: [.alert, .badge, .sound],
+        categories: nil
+      )
+      application.registerUserNotificationSettings(settings)
+    }
+
+    application.registerForRemoteNotifications()
+
+    Messaging.messaging().delegate = self
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// Add Firebase messaging delegate methods
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: Notification.Name("FCMToken"),
+      object: nil,
+      userInfo: dataDict
+    )
   }
 }
